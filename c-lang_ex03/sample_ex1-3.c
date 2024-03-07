e0481452186f8c924449bc3ea742afa6d9891919
Rikuto Nishiyama <crispycrack689@gmail.com>
2024-03-07 23:00:08 +0900

    Acquired advanced 1
    random and math

---
+++ c-lang_ex03/sample_ex1-3.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+	int n = -2;
+	double d1 = -2.5, d2 = 4.0;
+	printf("%dの絶対値は%dです\n", n, abs(n));
+	printf("%fの絶対値は%fです\n", d1, fabs(d1));
+	printf("%fの2乗は%fです\n", d2, pow(d2, 2));
+	printf("%fの平方根は%fです\n", d2, sqrt(d2));
+}
