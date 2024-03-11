e44160315025c77736ccb4da4b3611d9e374b9fd
Rikuto Nishiyama <crispycrack689@gmail.com>
2024-03-11 14:42:40 +0900

    Acquired advanced 2
    pointer

---
+++ c-lang_ex07/sample_ex2-3.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+void swap(int*, int*);
+
+void main()
+{
+	int a = 1, b = 2;
+	printf("a = %d, b = %d\n", a, b);
+	swap(&a, &b);
+	printf("a = %d, b = %d\n", a, b);
+}
+
+/// <summary>
+/// 変数の値の入れ替えを行う関数
+/// </summary>
+/// <param name="num1">入れ替え1</param>
+/// <param name="num2">入れ替え2</param>
+void swap(int* num1, int* num2)
+{
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = temp;
+}
