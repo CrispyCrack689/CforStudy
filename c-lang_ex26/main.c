ce379d33d2b82c9f6c52de5b2a073ea4b7ae3f1b
Rikuto Nishiyama <crispycrack689@gmail.com>
2024-03-24 19:15:19 +0900

    Acquired advanced 7
    complex script

---
+++ c-lang_ex26/main.c
@@ -0,0 +1,23 @@
+﻿#include "studentDatabase.h"
+#include "dataOutput.h"
+#include <stdio.h>
+
+void main()
+{
+	int i;
+	char names[][LENGTH] = { "山田太郎","太田美智子","大山次郎","山口さやか" };
+	int ids[] = { 1,2,2,3 };
+	initDatabase();
+	// データの登録
+	for (i = 0; i < 4; i++)
+	{
+		add(ids[i], names[i]);
+		printf("登録:%d,%s\n", ids[i], names[i]);
+		showError();
+	}
+	// 登録したデータの出力
+	for (i = 0; i < 3; i++)
+	{
+		showStudentData(get(i + 1));
+	}
+}
---
+++ c-lang_ex26/studentDatabase.c
@@ -0,0 +1,56 @@
+﻿#include "studentDatabase.h"
+#include <string.h>
+
+#define MESSAGE_LENGTH 256
+
+// データベースに登録されている学生の数
+static int num = 0;
+// 学生のデータベース
+static student student_database[MAX_STUDENT];
+// エラーメッセージ
+int error = MESSAGE_OK;
+
+// データベースの初期化
+void initDatabase()
+{
+	int i;
+	for (i = 0; i < MAX_STUDENT; i++)
+	{
+		student_database[i].id = -1;
+		strcpy(student_database[i].name, "");
+	}
+	error = MESSAGE_OK;	// エラーメッセージのクリア
+	num = 0;			// 登録された学生の数を0に初期化
+}
+
+// データベースへのデータの登録(学生番号、名前)
+int add(int id, char* name)
+{
+	// すでに登録されているidであれば、登録しない
+	if (get(id) == NULL && num < MAX_STUDENT)
+	{
+		student_database[num].id = id;
+		strcpy(student_database[num].name, name);
+		num++;
+		error = MESSAGE_OK;
+		return 1;	// 登録できたら、1を返す
+	}
+	error = MESSAGE_ERROR;
+	return 0;		// 登録できなければ、0を返す
+}
+
+// 学生のデータの取得
+student* get(int id)
+{
+	int i;
+	for (i = 0; i < num; i++)
+	{
+		// 該当するidのデータが見つかったら
+		// ポインタを返す
+		if (student_database[i].id == id)
+		{
+			return &student_database[i];
+		}
+	}
+	return NULL;
+}
